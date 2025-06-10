95ccd983c8e08c4d31d8c168e566840abaf39169
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2025-06-10 19:20:10 +0100

    Moved files to root

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: c:\Users\user\Documents\PSJ system\Web3Bridge Test\tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths based on your project structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
